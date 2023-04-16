0cfb8204becddf3dde4d4d9c977f5dfbcb720eff
Damini <daminijadhav8103@gmail.com>
2023-04-17 00:02:30 +0530

    commit3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">Amazon</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured Product</h1>
+        <div className="Products">
+          {data.products.map((product) => (
+            <div className="Product" key={product.slug}>
+              <img src={product.image} alt={product.name} />
+              <div className="product-info">
+                <p>{product.name}</p>
+                <p>{product.price}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
