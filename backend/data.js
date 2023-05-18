4685a0371ed047a4bf3ae7c2674f03a163f0dd35
Damini <daminijadhav8103@gmail.com>
2023-05-18 15:05:38 +0530

    commit12

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'Damini',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Pratik',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       // _id: '1',
